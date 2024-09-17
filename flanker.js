b4613877efc7d7a62454aa74273001b0425adac4
vaishsRP <12avaishanavimehta@gmail.com>
2024-09-17 14:29:36 +0530

    Update flanker.js

--- flanker.js
+++ flanker.js
@@ -1,135 +1,119 @@
-var jsPsych = initJsPsych({
-  on_finish: function() {
-    jsPsych.data.displayData();
-  }
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Flanker Task</title>
+    <script src="../packages/jspsych/dist/index.browser.js"></script>
+    <script src="../packages/plugin-html-keyboard-response/dist/index.browser.js"></script>
+    <script src="../packages/plugin-image-keyboard-response/dist/index.browser.js"></script>
+    <script src="../packages/plugin-preload/dist/index.browser.js"></script>
+    <link rel="stylesheet" href="../packages/jspsych/css/jspsych.css">
+  </head>
+  <body></body>
+  <script>
 
-/* experiment parameters */
-var reps_per_trial_type = 4;
-
-/* set up welcome block */
-var welcome = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: "Welcome to the experiment. Press any key to begin."
-};
-
-/* set up instructions block */
-var instructions = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: "<p>In this task, you will see five arrows on the screen, like the example below.</p>" +
-    "<img src='img/inc1.png'></img>" +
-    "<p>Press the left arrow key if the middle arrow is pointing left. (<)</p>" +
-    "<p>Press the right arrow key if the middle arrow is pointing right. (>)</p>" +
-    "<p>Enter a unique ID to start the experiment:</p>" +
-    "<input type='text' id='unique_id' />" +
-    "<p>Press any key to begin.</p>",
-  on_finish: function() {
-    // Store unique ID in jsPsych data
-    jsPsych.data.addProperties({
-      unique_id: document.getElementById('unique_id').value
-    });
-  },
-  post_trial_gap: 1000
-};
-
-/* defining stimuli */
-var test_stimuli = [
-  {
-    stimulus: "img/con1.png",
-    data: { stim_type: 'congruent', direction: 'left'}
-  },
-  {
-    stimulus: "img/con2.png",
-    data: { stim_type: 'congruent', direction: 'right'}
-  },
-  {
-    stimulus: "img/inc1.png",
-    data: { stim_type: 'incongruent', direction: 'right'}
-  },
-  {
-    stimulus: "img/inc2.png",
-    data: { stim_type: 'incongruent', direction: 'left'}
-  }
-];
-
-/* defining test timeline */
-var test = {
-  timeline: [{
-    type: jsPsychImageKeyboardResponse,
-    choices: ['ArrowLeft', 'ArrowRight'],
-    trial_duration: 1500,
-    stimulus: jsPsych.timelineVariable('stimulus'),
-    data: jsPsych.timelineVariable('data'),
-    on_finish: function(data) {
-      var correct = false;
-      if (data.direction == 'left' && jsPsych.pluginAPI.compareKeys(data.response, 'ArrowLeft') && data.rt > -1) {
-        correct = true;
-      } else if (data.direction == 'right' && jsPsych.pluginAPI.compareKeys(data.response, 'ArrowRight') && data.rt > -1) {
-        correct = true;
+    var jsPsych = initJsPsych({
+      on_finish: function() {
+        jsPsych.data.displayData();
       }
-      data.correct = correct;
+    });
 
-      // Send data to Google Sheets
-      sendDataToGoogleSheets(data.unique_id, data.rt, data.correct);
-    },
-    post_trial_gap: function() {
-        return Math.floor(Math.random() * 1500) + 500;
-    }
-  }],
-  timeline_variables: test_stimuli,
-  sample: {type: 'fixed-repetitions', size: reps_per_trial_type}
-};
+    /* experiment parameters */
+    var reps_per_trial_type = 4;
 
-/* defining debriefing block */
-var debrief = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: function() {
-    var total_trials = jsPsych.data.get().filter({trial_type: 'image-keyboard-response'}).count();
-    var accuracy = Math.round(jsPsych.data.get().filter({correct: true}).count() / total_trials * 100);
-    var congruent_rt = Math.round(jsPsych.data.get().filter({correct: true, stim_type: 'congruent'}).select('rt').mean());
-    var incongruent_rt = Math.round(jsPsych.data.get().filter({correct: true, stim_type: 'incongruent'}).select('rt').mean());
-    return "<p>You responded correctly on <strong>"+accuracy+"%</strong> of the trials.</p> " +
-    "<p>Your average response time for congruent trials was <strong>" + congruent_rt + "ms</strong>.</p>"+
-    "<p>Your average response time for incongruent trials was <strong>" + incongruent_rt + "ms</strong>.</p>"+
-    "<p>Press any key to complete the experiment. Thank you!</p>";
-  }
-};
+    /*set up welcome block*/
+    var welcome = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: "Welcome to the experiment. Press any key to begin."
+    };
 
-// manually preload images due to presenting them with timeline variables
-var images = ["img/con1.png","img/con2.png","img/inc1.png","img/inc2.png"];
-var preload = {
-  type: jsPsychPreload,
-  images: images
-};
+    /*set up instructions block*/
+    var instructions = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: "<p>In this task, you will see five arrows on the screen, like the example below.</p>"+
+        "<img src='img/inc1.png'></img>"+
+        "<p>Press the left arrow key if the middle arrow is pointing left. (<)</p>"+
+        "<p>Press the right arrow key if the middle arrow is pointing right. (>)</p>"+
+        "<p>Press any key to begin.</p>",
+      post_trial_gap: 1000
+    };
 
-/* set up experiment structure */
-var timeline = [];
-timeline.push(preload);
-timeline.push(welcome);
-timeline.push(instructions);
-timeline.push(test);
-timeline.push(debrief);
+    /*defining stimuli*/
+    var test_stimuli = [
+      {
+        stimulus: "img/con1.png",
+        data: { stim_type: 'congruent', direction: 'left'}
+      },
+      {
+        stimulus: "img/con2.png",
+        data: { stim_type: 'congruent', direction: 'right'}
+      },
+      {
+        stimulus: "img/inc1.png",
+        data: { stim_type: 'incongruent', direction: 'right'}
+      },
+      {
+        stimulus: "img/inc2.png",
+        data: { stim_type: 'incongruent', direction: 'left'}
+      }
+    ];
 
-/* start experiment */
-jsPsych.run(timeline);
+    /* defining test timeline */
+    var test = {
+      timeline: [{
+        type: jsPsychImageKeyboardResponse,
+        choices: ['ArrowLeft', 'ArrowRight'],
+        trial_duration: 1500,
+        stimulus: jsPsych.timelineVariable('stimulus'),
+        data: jsPsych.timelineVariable('data'),
+        on_finish: function(data) {
+          var correct = false;
+          if (data.direction == 'left' && jsPsych.pluginAPI.compareKeys(data.response, 'ArrowLeft') && data.rt > -1) {
+            correct = true;
+          } else if (data.direction == 'right' && jsPsych.pluginAPI.compareKeys(data.response, 'ArrowRight') && data.rt > -1) {
+            correct = true;
+          }
+          data.correct = correct;
+        },
+        post_trial_gap: function() {
+            return Math.floor(Math.random() * 1500) + 500;
+        }
+      }],
+      timeline_variables: test_stimuli,
+      sample: {type: 'fixed-repetitions', size: reps_per_trial_type}
+    };
 
-function sendDataToGoogleSheets(uniqueID, responseTime, accuracy) {
-  const url = 'https://script.google.com/macros/s/AKfycbygZUU3JfIeDfRPugQlTYUze-Xao34ZAVfDy3KvNwKx33ibKoc_-UTqW9Dfe1JKyFoy/exec'; // Replace with your Google Apps Script web app URL
-  
-  const data = {
-    "ID": uniqueID,
-    "responseTime": responseTime,
-    "accuracy": accuracy
-  };
+    /*defining debriefing block*/
+    var debrief = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: function() {
+        var total_trials = jsPsych.data.get().filter({trial_type: 'image-keyboard-response'}).count();
+        var accuracy = Math.round(jsPsych.data.get().filter({correct: true}).count() / total_trials * 100);
+        var congruent_rt = Math.round(jsPsych.data.get().filter({correct: true, stim_type: 'congruent'}).select('rt').mean());
+        var incongruent_rt = Math.round(jsPsych.data.get().filter({correct: true, stim_type: 'incongruent'}).select('rt').mean());
+        return "<p>You responded correctly on <strong>"+accuracy+"%</strong> of the trials.</p> " +
+        "<p>Your average response time for congruent trials was <strong>" + congruent_rt + "ms</strong>.</p>"+
+        "<p>Your average response time for incongruent trials was <strong>" + incongruent_rt + "ms</strong>.</p>"+
+        "<p>Press any key to complete the experiment. Thank you!</p>";
+      }
+    };
 
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data), // Ensure data is being sent as JSON
-    headers: {
-      'Content-Type': 'application/json', // Set the correct content type
-    },
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch(error => console.error('Error:', error));
-}
+    // manually preload images due to presenting them with timeline variables
+    var images = ["img/con1.png","img/con2.png","img/inc1.png","img/inc2.png"];
+    var preload = {
+      type: jsPsychPreload,
+      images: images
+    };
+
+    /*set up experiment structure*/
+    var timeline = [];
+    timeline.push(preload);
+    timeline.push(welcome);
+    timeline.push(instructions);
+    timeline.push(test);
+    timeline.push(debrief);
+
+    /*start experiment*/
+    jsPsych.run(timeline);
+    
+  </script>
+</html>
